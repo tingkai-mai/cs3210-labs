9203aa18c6a3dfb52720fb5c02cfaf53e1f0824d
Mai Ting Kai <wheatgrass.mc@gmail.com>
2025-09-08 15:27:18 +0800

    Draft: Initial boilerplate to run mm-omp.cpp for ex11

--- Lab2/submission/omp-job.sh
+++ Lab2/submission/omp-job.sh
@@ -2,17 +2,33 @@
 
 ## This is a Slurm job script for Lab 2: mm-omp.cpp
 
-#SBATCH --job-name=lab2-mmomp
+#SBATCH --job-name=lab2-ex11-e0694444
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=xs-4114
 #SBATCH --mem=1gb
 #SBATCH --time=00:03:00
-#SBATCH --output=lab2_mmomp_%j.slurmlog
-#SBATCH --error=lab2_mmomp_%j.slurmlog
+#SBATCH --output=logs/%j_lab2_ex11-e0694444.slurmlog
+#SBATCH --error=logs/%j_error_lab2_ex11-e0694444.slurmlog
 #SBATCH --mail-type=NONE
 
 # Check that two arguments were passed (matrix size and number of openmp threads)
+#if [ ! "$#" -eq 2 ]
+#then
+#  echo "Expecting 2 arguments (<matrix size> <num threads>), got $#"
+#  exit 1
+#fi
+
+events="cycles,instructions,duration_time,\
+fp_arith_inst_retired.scalar_double,\
+fp_arith_inst_retired.128b_packed_double,\
+fp_arith_inst_retired.256b_packed_double,\
+fp_arith_inst_retired.512b_packed_double,\
+fp_arith_inst_retired.scalar_single,\
+fp_arith_inst_retired.128b_packed_single,\
+fp_arith_inst_retired.256b_packed_single,\
+fp_arith_inst_retired.512b_packed_single"
+
 if [ ! "$#" -eq 2 ]
 then
   echo "Expecting 2 arguments (<matrix size> <num threads>), got $#"
@@ -24,11 +40,12 @@ echo "We are running on $(hostname)"
 echo "Job started at $(date)"
 echo "Arguments to your executable: $@"
 
-# Compile your code in case you forgot
 echo "Compiling..."
 srun g++ -fopenmp -o mm-omp mm-omp.cpp
-# Runs your script with the arguments you passed in
-echo "Running..."
-srun perf stat ./mm-omp $@
+
+echo "Running perf stat..."
+
+# Run 10 times
+perf stat -x , -r 10 -e "$events" -- ./mm-omp $@ 1>/dev/null 2>perf.tmp
 
 echo "Job ended at $(date)"
